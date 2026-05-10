34197e464ecadb7a8cea1f17b715203ccc2e04a1
Raja-Abdul-Sami <ra.sami.dev@gmail.com>
2026-05-10 15:06:57 +0500

    Added tests and CI

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ecommerce_test',
-  password: 'Rajasami',
-  port: 5432,
+  user: process.env.PGUSER || 'postgres',
+  host: process.env.PGHOST || 'localhost',
+  database: process.env.PGDATABASE || 'ecommerce_test',
+  password: process.env.PGPASSWORD || 'Rajasami',
+  port: process.env.PGPORT || 5432,
 });
 
 module.exports = pool;
